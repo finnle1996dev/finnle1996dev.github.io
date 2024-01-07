c81086ed026992492ce49c475f3fd3d086fefe19
finnle1996dev <119732559+finnle1996dev@users.noreply.github.com>
2024-01-07 07:48:21 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ var config_stk = ['mbbank', 'momo','vietinbank','vietcombank'];
 var set_1 = {
     fullname: 'Lương Đức Kiệt',
     username: 'DucKietDzz',
-    website: 'finnle1996dev.github.io',
+    website: 'https://finnle1996dev.github.io/giangsinh.github.io/',
     zalo: '09750170xx',
     avatar: 'https://img.upanh.tv/2023/12/17/F1A4CED5-18E2-46AA-8EBE-97B5DD195562.png',
     facebook: 'https://fb.com/ad.duckietdz',
